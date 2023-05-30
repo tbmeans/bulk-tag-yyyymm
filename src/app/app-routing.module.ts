159d6dc4bca583d985d946abb878c1fd60ffaa78
Timothy Means <tbmeans2005@gmail.com>
2023-05-30 01:59:21 -0400

    routes defined by varied button forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleButtonFormComponent } from './single-button-form/single-button-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SingleButtonFormComponent,
+    data: {
+      buttonText: {
+        labeling: 'Connect with Pocket to get started!',
+        id: 'auth-btn',
+        on: 'Login'
+      }
+    }
+  },
+  {
+    path: 'mypocket',
+    component: SingleButtonFormComponent,
+    data: {
+      buttonText: {
+        labeling: 'Find most recent saves not tagged YYYYMM',
+        id: 'get-btn',
+        on: 'Search'
+      }
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
